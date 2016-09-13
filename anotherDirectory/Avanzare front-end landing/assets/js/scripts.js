eb535940894e8be21faaea1fe47f981080cb8dc5
techfusion-hyd <techfusionhyd@gmail.com>
2016-09-13 19:51:17 +0530

    Added loader site template

---
+++ anotherDirectory/Avanzare front-end landing/assets/js/scripts.js
@@ -0,0 +1,1 @@
+$(window).load(function(){setTimeout(function(){$('#home').addClass('animated fadeInUp');$('#home').css('border-top','40px solid #fff').css('border-bottom','40px solid #fff')},600);$('#home-content img').addClass('animated infinite pulse');});
